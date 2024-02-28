282d4e17861fccbed582872a39703d41aad82fae
Saber <isaber.altria@gmail.com>
2024-02-29 00:42:15 +0800

    modify readme

---
+++ user/find.c
@@ -0,0 +1,12 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, int argv * [])
+{   
+    if(argc != 2)
+    {
+        printf("Error!\n");
+        exit(1);
+    }
+    
+}
